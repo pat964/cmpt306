e25c54a08d0c11784a8665ae117fff0132b1e4b0
pat964 <pat964@mail.usask.ca>
2015-10-01 16:59:11 -0600

    Started building project skeleton

---
+++ SpaceGame/Assets/Scripts/DeedCardScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class DeedCardScript : MonoBehaviour {
+
+	public Toolbox.CardType cardType;
+	public List<Toolbox.ActionType> actionTypes;
+	public Toolbox.CardColour cardColour;
+
+	public int cardNumber;
+	public string cardName;
+	public string topText;
+	public string bottomText;
+
+	public CardAction topAction;
+	public CardAction bottomAction;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class CardAction {
+	//when we get a better idea of how this will work, we'll put the details here
+}
---
+++ SpaceGame/Assets/Scripts/manager.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Toolbox : Singleton<Toolbox> {
+	protected Toolbox () {} // guarantee this will be always a singleton only - can't use the constructor!
+
+	public enum CardType{Action, DMD, Artifact};
+	public enum ActionType{Move, Influence, Combat, Heal, Special, Action};
+	public enum CardColour{Red, Blue, Green, White, Artifact};
+	
+	public string myGlobalVar = "whatever";
+	
+	public Language language = new Language();
+	
+	void Awake () {
+		// Your initialization code here
+	}
+}
+
+[System.Serializable]
+public class Language {
+	public string current;
+	public string lastLang;
+}
+
+/// <summary>
+/// Be aware this will not prevent a non singleton constructor
+///   such as `T myT = new T();`
+/// To prevent that, add `protected T () {}` to your singleton class.
+/// 
+/// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T _instance;
+	
+	private static object _lock = new object();
+	
+	public static T Instance
+	{
+		get
+		{
+			if (applicationIsQuitting) {
+				Debug.LogWarning("[Singleton] Instance '"+ typeof(T) +
+				                 "' already destroyed on application quit." +
+				                 " Won't create again - returning null.");
+				return null;
+			}
+			
+			lock(_lock)
+			{
+				if (_instance == null)
+				{
+					_instance = (T) FindObjectOfType(typeof(T));
+					
+					if ( FindObjectsOfType(typeof(T)).Length > 1 )
+					{
+						Debug.LogError("[Singleton] Something went really wrong " +
+						               " - there should never be more than 1 singleton!" +
+						               " Reopenning the scene might fix it.");
+						return _instance;
+					}
+					
+					if (_instance == null)
+					{
+						GameObject singleton = new GameObject();
+						_instance = singleton.AddComponent<T>();
+						singleton.name = "(singleton) "+ typeof(T).ToString();
+						
+						DontDestroyOnLoad(singleton);
+						
+						Debug.Log("[Singleton] An instance of " + typeof(T) + 
+						          " is needed in the scene, so '" + singleton +
+						          "' was created with DontDestroyOnLoad.");
+					} else {
+						Debug.Log("[Singleton] Using instance already created: " +
+						          _instance.gameObject.name);
+					}
+				}
+				
+				return _instance;
+			}
+		}
+	}
+	
+	private static bool applicationIsQuitting = false;
+	/// <summary>
+	/// When Unity quits, it destroys objects in a random order.
+	/// In principle, a Singleton is only destroyed when application quits.
+	/// If any script calls Instance after it have been destroyed, 
+	///   it will create a buggy ghost object that will stay on the Editor scene
+	///   even after stopping playing the Application. Really bad!
+	/// So, this was made to be sure we're not creating that buggy ghost object.
+	/// </summary>
+	public void OnDestroy () {
+		applicationIsQuitting = true;
+	}
+}
+
+static public class MethodExtensionForMonoBehaviourTransform {
+	/// <summary>
+	/// Gets or add a component. Usage example:
+	/// BoxCollider boxCollider = transform.GetOrAddComponent<BoxCollider>();
+	/// </summary>
+	static public T GetOrAddComponent<T> (this Component child) where T: Component {
+		T result = child.GetComponent<T>();
+		if (result == null) {
+			result = child.gameObject.AddComponent<T>();
+		}
+		return result;
+	}
+}
