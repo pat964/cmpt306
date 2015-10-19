6b8a72e7735e6a61bb3bfcac6e6bd0dab446b939
pat964 <pat964@mail.usask.ca>
2015-10-19 15:20:27 -0600

    Space names for enemies

--- SpaceGame/Assets/Scripts/EnemyScript.cs
+++ SpaceGame/Assets/Scripts/EnemyScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class EnemyScript : MonoBehaviour {
+	public string enemyName;
 	public Toolbox.EnemyColour colour;
 	public int armor;
 	public List<Toolbox.EnemyAttack> attacks;
