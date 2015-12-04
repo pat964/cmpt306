7503cb4d35a7cd10d557ab96da6a50b51213221f
krg028 <kaitlyngorsalitz@gmail.com>
2015-12-03 19:00:51 -0600

    Added energy dice tooltips

--- SpaceGame/Assets/Scripts/Tooltips/EnergySourceTooltip.cs
+++ SpaceGame/Assets/Scripts/Tooltips/EnergySourceTooltip.cs
@@ -11,41 +11,31 @@ public class EnergySourceTooltip : MonoBehaviour {
 	private string currentToolTipText = "";
 	private GUIStyle guiStyleFore;
 	private GUIStyle guiStyleBack;
-	private int delay; // the delay time for the tooltip to appear
-	private const int TIP_DELAY = 75; // time until the tooltip appears
-	private const int HELP_DELAY = 125; // time until additional help is added
 
 	public void Start()
 	{
-		toolTipText = "ENERGY TYPE: " + this.GetComponent<energyDiceScript> ().colour;
-		// set up
+		toolTipText = "\nYou can use an energy source to power up a card of " +
+			"the same colour. Only one can be used per turn, and once used a new " +
+			"one will take its place. They are shuffled at the end of each turn.";
+				// set up
 		guiStyleFore = new GUIStyle();
 		guiStyleFore.normal.textColor = Color.white;
-		guiStyleFore.alignment = TextAnchor.UpperCenter ;
+		guiStyleFore.alignment = TextAnchor.UpperLeft;
 		guiStyleFore.wordWrap = true;
 		guiStyleBack = new GUIStyle();
 		guiStyleBack.normal.textColor = Color.black;
-		guiStyleBack.alignment = TextAnchor.UpperCenter ;
+		guiStyleBack.alignment = TextAnchor.UpperLeft;
 		guiStyleBack.wordWrap = true;
 	}
 	
 	// Shows the tool tip after a delay because we may not want to see hex data all the time
-	public void OnMouseOver() {
-		delay++;
-		if (delay > HELP_DELAY) { // if user is looking at this long enough, add help text
-			currentToolTipText = toolTipText + "\nYou can use an energy source to power " +
-				"up a card of the same colour. Only one can be used per turn, and once used a new one" +
-				"will take its place.";
-		}
-		else if (delay > TIP_DELAY) { // else just show regular text
-			currentToolTipText = toolTipText;
-		}
+	public void OnPointerOver() {
+		currentToolTipText = toolTipText;
 	}
 	
 	// resets the delay and tool text when mouse event finishes
-	public void OnMouseExit ()
+	public void OnPointerExit ()
 	{
-		delay = 0;
 		currentToolTipText = "";
 	}
 	
@@ -56,8 +46,8 @@ public class EnergySourceTooltip : MonoBehaviour {
 		{
 			float x = Event.current.mousePosition.x;
 			float y = Event.current.mousePosition.y;
-			GUI.Label (new Rect (x-149,y+21,300,60), currentToolTipText, guiStyleBack);
-			GUI.Label (new Rect (x-150,y+20,300,60), currentToolTipText, guiStyleFore);
+			GUI.Label (new Rect (x-21,y+21,200,100), currentToolTipText, guiStyleBack);
+			GUI.Label (new Rect (x-20,y+20,200,100), currentToolTipText, guiStyleFore);
 		}
 	}
 }
