570c941aa5bbb1cd41e878aa3e597803ab63043a
krg028 <kaitlyngorsalitz@gmail.com>
2015-12-03 20:07:26 -0600

    Corrected energy source tooltips

--- SpaceGame/Assets/Scripts/Tooltips/EnergySourceTooltip.cs
+++ SpaceGame/Assets/Scripts/Tooltips/EnergySourceTooltip.cs
@@ -11,9 +11,13 @@ public class EnergySourceTooltip : MonoBehaviour {
 	private string currentToolTipText = "";
 	private GUIStyle guiStyleFore;
 	private GUIStyle guiStyleBack;
+	private bool displayTip;
+	private int delay; // the delay time for the tooltip to appear
+	private const int TIP_DELAY = 75; // time until the tooltip appears
 
 	public void Start()
 	{
+		displayTip = false;
 		toolTipText = "\nYou can use an energy source to power up a card of " +
 			"the same colour. Only one can be used per turn, and once used a new " +
 			"one will take its place. They are shuffled at the end of each turn.";
@@ -27,15 +31,26 @@ public class EnergySourceTooltip : MonoBehaviour {
 		guiStyleBack.alignment = TextAnchor.UpperLeft;
 		guiStyleBack.wordWrap = true;
 	}
-	
+
+	public void Update() {
+		if (displayTip) {
+			delay++;
+		}
+		if (delay > TIP_DELAY) {
+			currentToolTipText = toolTipText;
+		}
+	}
+
 	// Shows the tool tip after a delay because we may not want to see hex data all the time
 	public void OnPointerOver() {
-		currentToolTipText = toolTipText;
+		displayTip = true;
 	}
-	
+
 	// resets the delay and tool text when mouse event finishes
 	public void OnPointerExit ()
 	{
+		delay = 0;
+		displayTip = false;
 		currentToolTipText = "";
 	}
 	
